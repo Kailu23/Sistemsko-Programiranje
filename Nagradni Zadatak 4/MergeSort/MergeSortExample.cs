81c813edc4daf1c5cf0a941c6ab0cc1efa8827a3
Marin Valenčak <valencakmarin@gmail.com>
2025-11-14 03:52:57 +0100

    Add MergeSort, MergoSortExample and MergeSortMultiThreaded .cs files
                - MergeSort is single threaded implementation algorithm
                - MergoSortExample is for testing the single threaded algorithm
                - MergoSortMultiThreaded is empty; implementation needed
    	    - Program.cs has basic code for running the examples

---
+++ Nagradni Zadatak 4/MergeSort/MergeSortExample.cs
@@ -0,0 +1,25 @@
+﻿using Nagradni_Zadatak_4.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nagradni_Zadatak_4.MergeSort
+{
+    class MergeSortExample : IExample
+    {
+        public void Run()
+        {
+            int[] numbers = new int[] { 38, 27, 43, 3, 9, 82, 10 };
+
+            MergeSort.MergeSortSingleThread(numbers, 0, numbers.Length - 1);
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Nagradni Zadatak 4/MergeSort/MergeSortMultiThreaded.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Xml.Schema;
+
+namespace Nagradni_Zadatak_4.MergeSort
+{
+    public class MergeSortMultiThreaded
+    {
+
+    }
+}
