b0a9315812c301c7d08590195425fa898409bee6
Marin Valenčak <valencakmarin@gmail.com>
2025-11-30 00:45:14 +0100

    Add QuickSort empty class

---
+++ Nagradni Zadatak 4/QuickSort/QuickSort.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nagradni_Zadatak_4.QuickSort
+{
+    public class QuickSort
+    {
+    }
+}
